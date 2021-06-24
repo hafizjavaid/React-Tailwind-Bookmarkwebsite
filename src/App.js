50d73ca754952471b6f9d0ec46c27b07f8026d92
hafizjavaid <hafizjaidi786@gmail.com>
2021-06-24 13:27:22 +0500

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="font-serif">
+      <header>
+      <nav className="container flex items-center py-4 mt-4 sm:mt-12">
+        <div className="py-1">
+          {/* <img src="./imgs/logo-bookmark.svg" alt="" /> */}
+          </div>
+        <ul className="hidden sm:flex flex-1 justify-end items-center gap-12 text-bookmark-blue uppercase text-xs">
+          <li className="cursor-pointer">Features</li>
+          <li className="cursor-pointer">Pricing</li>
+          <li className="cursor-pointer">Contact</li>
+          <button type="button" className="bg-bookmark-red text-white rounded-md px-7 py-3 uppercase">Login</button>
+        </ul>
+        <div className="flex sm:hidden flex-1 justify-end">
+          <i className="text-2xl fas fa-bars"></i>
+        </div>
+      </nav>
+    </header>
     </div>
   );
 }
